dab0d729a5b2231b92832d5700baa21035f121a3
francisco <franciscojn49@gmail.com>
2019-08-27 00:25:42 -0300

    Sensor de proximidade (ok)

--- Cliente/alarmeAprox/app/src/main/java/com/example/alarmeaprox/MainActivity.java
+++ Cliente/alarmeAprox/app/src/main/java/com/example/alarmeaprox/MainActivity.java
@@ -2,11 +2,21 @@ package com.example.alarmeaprox;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
 //import android.support.v7.app.AppCompatActivity;
+import android.hardware.SensorEventListener;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+
+import android.hardware.SensorManager;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.widget.TextView;
+import android.hardware.SensorEventListener;
+
 
 import java.time.Instant;
 
@@ -14,11 +24,26 @@ public class MainActivity extends AppCompatActivity {
 
     public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
     MaskEditUtil mEU = new MaskEditUtil();
-
+    TextView data, ProximitySensor;
+    SensorManager mySensorManager;
+    Sensor myProximitySensor;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        data = (TextView) findViewById(R.id.data);
+
+        mySensorManager = (SensorManager) getSystemService(
+                Context.SENSOR_SERVICE);
+        myProximitySensor = mySensorManager.getDefaultSensor(
+                Sensor.TYPE_PROXIMITY);
+        if (myProximitySensor == null) {
+            ProximitySensor.setText("No Proximity Sensor!");
+        } else {
+            mySensorManager.registerListener(proximitySensorEventListener,
+                    myProximitySensor,
+                    SensorManager.SENSOR_DELAY_NORMAL);
+        }
     }
 
     public void sendMessage(View view){
@@ -28,4 +53,26 @@ public class MainActivity extends AppCompatActivity {
         intent.putExtra(EXTRA_MESSAGE, message);
         startActivity(intent);
     }
+
+    SensorEventListener proximitySensorEventListener
+            = new SensorEventListener() {
+        @Override
+        public void onAccuracyChanged(Sensor sensor, int accuracy) {
+            // TODO Auto-generated method stub
+        }
+        @Override
+        public void onSensorChanged(SensorEvent event) {
+            // TODO Auto-generated method stub
+            if (event.sensor.getType() == Sensor.TYPE_PROXIMITY) {
+                if (event.values[0] == 0) {
+                    data.setText("Near");
+//                    Log.d("sensor", "longe");
+                } else {
+                    data.setText("Away");
+//                    Log.d("sensor", "p/erto");
+                }
+            }
+        }
+    };
+
 }
